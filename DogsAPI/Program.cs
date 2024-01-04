8f3d3c7cff5e8d9a9f5d94082a702130e6ec7e7d
Karolina Polak <karolinapol2001@gmail.com>
2024-01-04 21:16:24 +0100

    disable cors

--- DogsAPI/Program.cs
+++ DogsAPI/Program.cs
@@ -22,6 +22,12 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+
+    app.UseCors(x => x
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+                .SetIsOriginAllowed(origin => true)
+                .AllowCredentials());
 }
 
 app.UseHttpsRedirection();
